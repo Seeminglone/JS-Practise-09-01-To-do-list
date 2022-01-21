e59ad09ff2270ecab137ccba4529fb640732ecc1
Seeminglone <yurakravtsov833@gmail.com>
2022-01-21 23:31:48 +0100

    SampleCode

--- script.js
+++ script.js
@@ -8,19 +8,18 @@ deletedBtn = document.querySelector('.deleted-tasks-btn');
 allTaskBtn = document.querySelector('.all-tasks-btn');
 let arrayWithCompletedTask = [];
 let arrayWithDeletedTask = []; 
-let arrayWithAllTask = []; 
+let arrayWithAllTask = [];
+let inputOurTask = document.getElementById('new-tasks'); 
 
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show-links");
 });
 
-
 addBtn.addEventListener('click', function(){
- 
-//Move from input to paragraph   
-
-    let inputOurTask = document.getElementById('new-tasks');
-    if (inputOurTask.value){ 
+    
+//Move from input to paragraph    
+           
+    if (inputOurTask.value){
         todoalltask.innerHTML += `<div class="task">
             <b><p class="paragraph">${inputOurTask.value}</p></b>
             <button class="completed-btn">
@@ -33,11 +32,31 @@ addBtn.addEventListener('click', function(){
             <i class="fas fa-edit"></i>
             </button>
             </div>`;
-             
-arrayWithAllTask.push(inputOurTask.value);
-console.log(arrayWithAllTask);
+                arrayWithAllTask.push(inputOurTask.value);
+                todoalltask.innerHTML = '';
+                arrayWithAllTask = arrayWithAllTask.filter( function( el ) {
+                    return arrayWithCompletedTask.indexOf( el ) < 0;
+                });
+                arrayWithAllTask = arrayWithAllTask.filter( function( el ) {
+                    return arrayWithDeletedTask.indexOf( el ) < 0;
+                });
+                arrayWithAllTask.forEach(element=>{
+                    todoalltask.innerHTML += `<div class="task">
+                        <b><p class="paragraph">${element}</p></b>
+                        <button class="completed-btn">
+                        <i class="fas fa-check"></i>
+                        </button>
+                        <button class="deleted-btn">
+                        <i class="fas fa-trash-alt"></i>
+                        </button>
+                        <button class="edit-btn">
+                        <i class="fas fa-edit"></i>
+                        </button>
+                        </div>`;
+                   
+            });
+            
 //Delete our tasks
-
 
         deleteBtns = document.querySelectorAll('.deleted-btn').forEach(btn => {
             btn.addEventListener('click', (event) => {
@@ -56,41 +75,61 @@ console.log(arrayWithAllTask);
                 event.currentTarget.parentElement.remove();
             });
         });
+        
+        allTaskBtn.addEventListener('click', function(){
+            todoalltask.innerHTML = '';
+            arrayWithAllTask.forEach(element=>{
+                todoalltask.innerHTML += `<div class="task">
+                    <b><p class="paragraph">${element}</p></b>
+                    <button class="completed-btn">
+                    <i class="fas fa-check"></i>
+                    </button>
+                    <button class="deleted-btn">
+                    <i class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="edit-btn">
+                    <i class="fas fa-edit"></i>
+                    </button>
+                    </div>`;
+                    arrayWithAllTask = arrayWithAllTask.filter( function( el ) {
+                        return arrayWithCompletedTask.indexOf( el ) < 0;
+                    });
+                    arrayWithAllTask = arrayWithAllTask.filter( function( el ) {
+                        return arrayWithDeletedTask.indexOf( el ) < 0;
+                    });
+                    //Delete our tasks
+                   
 
-        inputOurTask.value = '';
+                deleteBtns = document.querySelectorAll('.deleted-btn').forEach(btn => {
+                    btn.addEventListener('click', (event) => {
+                        let paragraphFromDeletedTask = event.currentTarget.parentElement.firstElementChild;
+                        arrayWithDeletedTask.push(paragraphFromDeletedTask.children[0].innerHTML);
+                    event.currentTarget.parentElement.remove();
+                    });
+                });
+
+// Acepted that we completed our task
+
+                completedBtns = document.querySelectorAll('.completed-btn').forEach(btn => {
+                    btn.addEventListener('click', (event) => {
+                    let paragraphFromComletedTask = event.currentTarget.parentElement.firstElementChild;
+                        arrayWithCompletedTask.push(paragraphFromComletedTask.children[0].innerHTML);
+                        event.currentTarget.parentElement.remove();
+                    });
+                    
+                });
+               
+            });
+            
+        });
+
+    inputOurTask.value = '';    
     }
     else {
         return;
     }
+   
 });
-
-
-
-// All task button, we check what all our task
-
-allTaskBtn.addEventListener('click', function(){
-    todoalltask.innerHTML = '';
-    arrayWithAllTask.forEach(element=>{
-        todoalltask.innerHTML += `<div class="task">
-            <b><p class="paragraph">${element}</p></b>
-            <button class="completed-btn">
-            <i class="fas fa-check"></i>
-            </button>
-            <button class="deleted-btn">
-            <i class="fas fa-trash-alt"></i>
-            </button>
-            <button class="edit-btn">
-            <i class="fas fa-edit"></i>
-            </button>
-            </div>`;
-    });
-});
-
-
-
-
-
-
 
 // Completed button, we check what we done
 
@@ -103,9 +142,7 @@ doneBtn.addEventListener('click', function(){
     });
 });
 
-
 // Deleted button, we check what we delete
-
 
 deletedBtn.addEventListener('click', function(){
     todoalltask.innerHTML = '';
